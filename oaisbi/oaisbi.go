91044100976379d272f276bf44bcd53bcd95605d
kharade <rohan.kharade@openairinterface.org>
2022-11-04 21:25:55 +0100

    Slice remove fixed

--- oaisbi/oaisbi.go
+++ oaisbi/oaisbi.go
@@ -9,40 +9,24 @@ import (
 	"encoding/json"
 	"bytes"
 	// "io/ioutil"
-
 )
 
 var SnssaiLocal common.SNSSAI
 
 func UpdateSnssai (Snssai common.SNSSAI) {
-	logger.OaiSbiLog.Infoln("Snssai updated (", Snssai.Sst,",", Snssai.Sd,")")
 	var amf_cfg OaiAamfConfig
 	GetAmfConfig(&amf_cfg)
-	SnssaiLocal = Snssai
 	AddSlice(Snssai, &amf_cfg)
+	logger.OaiSbiLog.Infoln("Snssai updated (", Snssai.Sst,",", Snssai.Sd,")")
 }
 
 func DeleteSnssai (Snssai common.SNSSAI) {
 	var amf_cfg OaiAamfConfig
 	GetAmfConfig(&amf_cfg)
 	SnssaiLocal = Snssai
-	Sd, _:= strconv.Atoi(Snssai.Sd)
-	
-	index := 0
-	del_index := 0
-	for _, snssai := range amf_cfg.PlmnList[0].SliceList {
-		if (snssai.Sst == Snssai.Sst && snssai.Sd == Sd) {
-			del_index = index
-		}
-		index++
-	}
-	amf_cfg.PlmnList[0].SliceList[del_index] = amf_cfg.PlmnList[0].SliceList[len(amf_cfg.PlmnList[0].SliceList)-1] // Copy last element to index i.
-	amf_cfg.PlmnList[0].SliceList[len(amf_cfg.PlmnList[0].SliceList)-1].Sst = 0   // Erase last element (write zero value).
-	amf_cfg.PlmnList[0].SliceList[len(amf_cfg.PlmnList[0].SliceList)-1].Sd = 0   // Erase last element (write zero value).
-	amf_cfg.PlmnList[0].SliceList = amf_cfg.PlmnList[0].SliceList[:len(amf_cfg.PlmnList[0].SliceList)-1] 
 
-	println(amf_cfg.PlmnList[0].SliceList)
-	
+	DeleteSnssaiFromList(&amf_cfg, Snssai)
+
 	reqbody, _ := json.Marshal(amf_cfg)
 
 	AmfConfigUpdate(reqbody)
@@ -71,8 +55,7 @@ func GetAmfConfig (amf_cfg *OaiAamfConfig) {
 }
 
 func AddSlice (Snssai common.SNSSAI, amf_cfg *OaiAamfConfig){
-// Add slice to AMF
-logger.OaiSbiLog.Errorln("Updating AMF slice")
+logger.OaiSbiLog.Debugln("Updating AMF slice")
 Sd, _:= strconv.Atoi(Snssai.Sd)
 
 for _, snssai := range amf_cfg.PlmnList[0].SliceList {
@@ -81,6 +64,7 @@ for _, snssai := range amf_cfg.PlmnList[0].SliceList {
 		return
 	}
 }
+DeleteSnssaiFromList(amf_cfg, SnssaiLocal)
 
 var Slice Slice
 Slice.Sst = Snssai.Sst
@@ -89,9 +73,8 @@ Slice.Sd = Sd
 amf_cfg.PlmnList[0].SliceList = append(amf_cfg.PlmnList[0].SliceList, Slice)
 reqbody, _ := json.Marshal(amf_cfg)
 
-// Push to AMF
-
-//logger.OaiSbiLog.Infoln("AMF config updated successfully: ", resp.Body)
+SnssaiLocal.Sst = Snssai.Sst
+SnssaiLocal.Sd = Snssai.Sd
 
 AmfConfigUpdate(reqbody)
 
@@ -119,4 +102,21 @@ func AmfConfigUpdate(reqbody []byte) {
 		panic(err)
 	}
     logger.OaiSbiLog.Infoln("AMF config updated successfully: ", resp.Body)
+}
+
+func DeleteSnssaiFromList (amf_cfg *OaiAamfConfig, Snssai common.SNSSAI){
+	index := 0
+	del_index := 255
+	Sd, _:= strconv.Atoi(Snssai.Sd)
+	for _, snssai := range amf_cfg.PlmnList[0].SliceList {
+		if (snssai.Sst == Snssai.Sst && snssai.Sd == Sd) {
+			del_index = index
+		}
+		index++
+	}
+
+	if del_index != 255{
+		amf_cfg.PlmnList[0].SliceList[del_index] = amf_cfg.PlmnList[0].SliceList[len(amf_cfg.PlmnList[0].SliceList)-1] // Copy last element to index i.
+		amf_cfg.PlmnList[0].SliceList = amf_cfg.PlmnList[0].SliceList[:len(amf_cfg.PlmnList[0].SliceList)-1] // Erase last element
+	}
 }
